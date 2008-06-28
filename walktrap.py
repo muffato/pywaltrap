94da09a4b04b7e39d25af39896f33c6325a39a16
Matthieu Muffato <muffato@biologie.ens.fr>
2008-06-28 01:00:00 +0100

    Restored from the incremental backup named full-20080628

--- walktrap.py
+++ walktrap.py
@@ -13,156 +13,48 @@
 #    - Le dendogramme (instance de WalktrapDendogram)
 
 
-import os
 import sys
 import _walktrap
-import utils.myMaths
 import utils.myTools
 
 
+# Lancer un walktrap a partir d'un graphe
+# Permet de detecter les composantes connexes
 ###############################################
-# Lancer un walktrap en bufferisant le graphe #
-# Permet de detecter les composantes connexes #
-###############################################
-class WalktrapLauncher:
 
-	def __init__(self, randomWalksLength=5, verboseLevel=0, showProgress=False, memoryUseLimit=0):
-		self.edges = utils.myTools.defaultdict(dict)
-		self.randomWalksLength = randomWalksLength
-		self.verboseLevel = verboseLevel
-		self.showProgress = showProgress
-		self.memoryUseLimit = memoryUseLimit
+def doWalktrap(edges, randomWalksLength=5, verboseLevel=0, showProgress=False, memoryUseLimit=0):
 
-	# Ajout d'une arete
-	####################
-	def addEdge(self, x, y, weight):
+	print >> sys.stderr, "Computing connected components ...",
+	# Les composantes connexes
+	combin = utils.myTools.myCombinator()
+	for (x,l) in edges.iteritems():
+		l.pop(x, None)
+		combin.addLink(l.keys() + [x])
 
-		weight = float(weight)
-		if weight <= 0:
-			return
+	print >> sys.stderr, "Launching walktrap ",
+	res = []
+	n = len(edges)
+	for nodes in combin:
+		# Reindexation des noeuds
+		indNodes = {}
+		for (i,node) in enumerate(nodes):
+			indNodes[node] = i
 
-		try:
-			x = int(x)
-		except ValueError:
-			pass
-		try:
-			y = int(y)
-		except ValueError:
-			pass
+		# On lance le walktrap
+		(relevantCuts,dend) = _walktrap.doWalktrap(indNodes, edges, randomWalksLength=randomWalksLength, verboseLevel=verboseLevel, showProgress=showProgress, memoryUseLimit=memoryUseLimit)
 
-		self.edges[x][y] = weight
-		self.edges[y][x] = weight
+		# On doit revenir aux noms de noeuds originels
+		def translate(x):
+			if x < len(nodes):
+				return nodes[x]
+			else:
+				return (x,)
+		dend = [(cut,tuple(translate(f) for f in fils),translate(pere)) for (cut,fils,pere) in dend]
+		res.append( (nodes, relevantCuts, dend, WalktrapDendogram(dend, nodes)) )
+		sys.stderr.write('.')
+	print >> sys.stderr, " OK"
 
-	# Ajout automatique depuis un fichier
-	######################################
-	def updateFromFile(self, f):
-		for l in f:
-			c = l.split()
-			self.addEdge(c[0], c[1], c[2])
-
-	# Ajout automatique depuis une fonction
-	########################################
-	def updateFromFunc(self, items, func):
-		for (x1,x2) in utils.myTools.myIterator.tupleOnStrictUpperList(items):
-			self.addEdge(x1, x2, func(x1, x2))
-
-	# Ajout automatique depuis des dictionnaires existants
-	#######################################################
-	def updateFromDict(self, d, items = None):
-		for x1 in d:
-			if (items != None) and (x1 not in items):
-				continue
-			for (x2,v) in self.edges[x1].iteritems():
-				if (items != None) and (x2 not in items):
-					continue
-				self.addEdge(x1, x2, v)
-
-
-	# Lancement du walktrap
-	########################
-	def doWalktrap(self):
-
-		print >> sys.stderr, "Computing connected components ...",
-		# Les composantes connexes
-		combin = utils.myTools.myCombinator()
-		for (x,l) in self.edges.iteritems():
-			combin.addLink(l.keys() + [x])
-
-		self.res = []
-
-		n = len(self.edges)
-		print >> sys.stderr, "Launching walktrap ",
-		for nodes in combin:
-			if len(nodes) != len(set(nodes)):
-				# Cette erreur se declenche si il y a une boucle d'un noeud sur lui-meme ?
-				print >> sys.stderr, "ERROR: BAD CONNECTED COMPONENT"
-			# Reindexation des noeuds
-			indNodes = {}
-			for (i,node) in enumerate(nodes):
-				#if node in self.edges:
-				#	for j in xrange(i):
-				#		if nodes[j] in self.edges[node]:
-				#			print j, i, self.edges[node][nodes[j]]
-				indNodes[node] = i
-
-			# On lance le walktrap
-			(relevantCuts,dend) = _walktrap.doWalktrap(indNodes, self.edges, randomWalksLength=self.randomWalksLength, verboseLevel=self.verboseLevel, showProgress=self.showProgress, memoryUseLimit=self.memoryUseLimit)
-
-			# On doit revenir aux noms de noeuds originels
-			def translate(x):
-				if x < len(nodes):
-					return nodes[x]
-				else:
-					return (x,)
-			dend = [(cut,tuple(translate(f) for f in fils),translate(pere)) for (cut,fils,pere) in dend]
-			self.res.append( (nodes, relevantCuts, dend, WalktrapDendogram(dend, nodes)) )
-			sys.stderr.write('.')
-		print >> sys.stderr, " OK"
-
-
-#################################################################################
-# Lancer un walktrap lorsqu'on sait qu'il n'y a qu'une seule composante connexe #
-#################################################################################
-class WalktrapDirectLauncher:
-
-	def __init__(self, randomWalksLength=5, verboseLevel=0, showProgress=False, memoryUseLimit=0):
-		self.edges = {}
-		s = '/users/ldog/muffato/work/scripts/utils/walktrap/walktrap -t%d -d2 -m%d' % (randomWalksLength, memoryUseLimit)
-		if showProgress:
-			(self.stdin,self.stdout) = os.popen2(s)
-		else:
-			(self.stdin,self.stdout,stderr) = os.popen3(s + " -s")
-			stderr.close()
-		self.nodes = set()
-
-	def addEdge(self, x, y, weight):
-		self.nodes.add(x)
-		self.nodes.add(y)
-		print >> self.stdin, x, y, weight
-
-	def updateFromFile(self, f):
-		for l in f:
-			c = l.split()
-			self.nodes.add(int(c[0]))
-			self.nodes.add(int(c[1]))
-			print >> self.stdin, l,
-
-	def updateFromFunc(self, items, func):
-		for (x1,x2) in utils.myTools.myIterator.tupleOnStrictUpperList(items):
-			score = func(x1, x2)
-			if score > 0:
-				self.addEdge(x1, x2, score)
-
-	def updateFromDict(self, d):
-		for x1 in d:
-			for (x2,v) in self.edges[x1].iteritems():
-				self.addEdge(x1, x2, v)
-
-	def doWalktrap(self):
-
-		self.stdin.close()
-		(relevantCuts,dend) = loadWalktrapOutput(self.stdout)
-		self.res = [(self.nodes, relevantCuts, dend, WalktrapDendogram(dend, self.nodes))]
+	return res
 
 
 ###################################################
@@ -240,6 +132,8 @@ class WalktrapDendogram:
 # Demande a l'utilisateur quelle partition choisir #
 ####################################################
 def askPartitionChoice(dend, cuts):
+
+	import utils.myMaths
 
 	def mystr( (alpha,relevance,(clusters,lonely)) ):
 		return "alpha=%f relevance=%f clusters=%d size=%d lonely=%d sizes={%s}" % \
